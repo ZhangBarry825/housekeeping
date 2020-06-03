730ed2f5fd9b190ce1e2934066d19147875261f2
Administrator <530027054@qq.com>
2020-06-03 14:36:08 +0800

    chooseImage

--- pages/join/join.js
+++ pages/join/join.js
@@ -7,6 +7,19 @@ Page({
   data: {
     address:'',
   },
+  chooseImage1(){
+    wx.chooseImage({
+      sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        console.log(res,987)
+        // that.setData({
+        //   files: that.data.files.concat(res.tempFilePaths)
+        // });
+      }
+    })
+  },
   selectMap(){
     let that = this
     wx.getLocation({
