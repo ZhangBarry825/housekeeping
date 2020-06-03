ea0c51a9fe3b1c8c296c20058d505dbae614b9c3
wangjun <1355183848@qq.com>
2020-06-03 14:45:17 +0800

    home 对接 添加授权

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,86 +1,206 @@
-// pages/home/home.js
-const app=getApp()
+/* pages/home/home.js */
+const app = getApp()
+const api = require('../../utils/api.js');
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 500
-  },
-  goItem(item){
-    let path;
-    let type=item.currentTarget.dataset.type
-    if(type=='logo'){
-       path = '/pages/service-item/service-item?id=' + item.currentTarget.dataset
-    }else if(type=='knowledge-list'){
-      path = '/pages/knowledge/knowledge?id='
-    }else if(type=='knowledge-detail'){
-      path = '/pages/knowledge-detail/knowledge-detail?id='
-    }
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        background: {},
+        lifeCommonSense: {},
+        navigations:{},
+        classmenulist: [],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: false,
+        interval: 2000,
+        duration: 500
+    },
+    goItem(item) {
+        let path;
+        let tn = item.currentTarget.dataset.type;
+        if (tn == 'knowledge-list') {
+            path = '/pages/knowledge/knowledge?id='
+        } else if (tn == 'knowledge-detail') {
+            path = '/pages/knowledge-detail/knowledge-detail?id='
+        } else if (tn == 'service-item') {
+            path = '/pages/service-item/service-item?id=' + item.currentTarget.dataset.id
+        } else if (tn == 'publish-task') {
+            path = '/pages/publish-task/one/one?id=' + item.currentTarget.dataset.id
+        }
+        //app.navigateTo(path);
+        wx.navigateTo({
+            url: path
+        })
+    },
 
-    console.log(item,123)
-    app.navigateTo(path)
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+        var that = this;
+        that.loadBanners();
+        that.lifeCommonSense();
+        that.navigations();
+        that.classMenuList();
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        var a1 = wx.getStorageSync('login');
+        var a2 = wx.getStorageSync('token');
+        var a3 = wx.getStorageSync('userid');
+        console.log(a1);
+        console.log(a2);
+        console.log(a3);
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /* 轮播图 */
+    loadBanners: function() {
+        var that = this;
+        api.get({
+            url: '/Home/poster/5',
+            data: {},
+            success: data => {
+                if(data.code == 200){
+                    var list = data.poster;
+                    if(list.length > 0){
+                        for(var i=0; i<list.length; i++){
+                            list[i].poster_pic = api.HOST +'/'+ list[i].poster_pic;
+                        }
+                        that.setData({
+                            background: list
+                        })
+                    }
+                }else{
+                    console.log('获取banner数据失败');
+                }
+            }
+        });
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /* 生活小常识 */
+    lifeCommonSense:function(){
+        var that = this;
+        api.get({
+            url: '/Article/Article_list/5/1/4',
+            data: {},
+            success: data => {
+                if(data.code == 200){
+                    var list = data.list;
+                    console.log(list)
+                    if(list.length > 0){
+                        that.setData({
+                            lifeCommonSense: data.list
+                        })
+                    }
+                }else{
+                    console.log('获取生活小常识数据失败');
+                }
 
-  }
+            }
+        });
+    },
+
+    /* 导航 */
+    navigations: function() {
+        var that = this;
+        api.get({
+            url: '/Home/demand_category/0',
+            data: {},
+            success: data => {
+                if(data.code == 200){
+                    var list = data.list;
+                    if(list.length > 0){
+                        for(var i=0; i<list.length; i++){
+                            list[i].icon = api.HOST +'/'+ list[i].icon;
+                        }
+                        that.setData({
+                            navigations: list
+                        })
+                    }
+                }else{
+                    console.log('获取导航数据失败');
+                }
+            }
+        });
+    },
+
+    /* 分类列表 */
+    classMenuList: function() {
+        var that = this;
+        api.get({
+            url: '/Home/demand_category/9999',
+            data: {},
+            success: data => {
+                if(data.code == 200){
+                    var list = data.list;
+                    if(list.length > 0){
+                        for(var i=0; i<list.length; i++){
+                            var icon = list[i];
+                            icon.icon = api.HOST +'/'+ icon.icon;
+                            if(icon.children.length > 0){
+                                for(var y=0; y<icon.children.length; y++){
+                                    icon.children[y].icon = api.HOST +'/'+ icon.children[y].icon;
+                                }
+                            }
+                        }
+                        that.setData({
+                            classmenulist: list
+                        })
+                    }
+                }else{
+                    console.log('获取导航数据失败');
+                }
+            }
+        });
+    },
+
+
+
 })
---
+++ pages/shouquan/shouquan.js
@@ -0,0 +1,34 @@
+const api = require('../../utils/api.js');
+Page({
+	data: {
+		canIUse: wx.canIUse('button.open-type.getUserInfo'),
+		hide: 1,
+		phone: '',
+		userid: ''
+	},
+	onLoad: function() {
+
+	},
+	/* 点击授权 */
+	bindGetUserInfo: function(e) {
+		var that = this;
+		that.btntap();
+		api.login(e);
+	},
+	btntap: function() {
+		/* 查看是否授权 */
+		wx.getSetting({
+			success: function(res) {
+				if (res.authSetting['scope.userInfo']) {
+					/* 已经授权，可以直接调用 getUserInfo 获取头像昵称 */
+					wx.getUserInfo({
+						lang: 'zh_CN',
+						success: function(res) {
+							console.log(res,'333');
+						}
+					})
+				}
+			}
+		});
+	}
+})
