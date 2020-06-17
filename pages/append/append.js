c0fd3157411f0e3da53b8752dd6fcb88377e7e54
Your Name <you@example.com>
2020-06-17 16:02:06 +0800

    补加金额

--- pages/append/append.js
+++ pages/append/append.js
@@ -1,13 +1,83 @@
 // pages/append/append.js
+const api = require('../../utils/api.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    renlist: {}
+    renlist: {},
+    amount: ""
   },
+  getCode (e) {
+    console.log(e.detail.value)
+    this.setData({
+      amount: e.detail.value
+    })
+  },
+  // 确认支付
+  weChatPay () {
+    // if(this.data.amount)
+    let that = this
+    api.post({
+      url: `/Order/insert_order_amount`,
+      data: {
+        user_id: wx.getStorageSync('userid'),
+        user_token: wx.getStorageSync('token'),
+        order_id: that.data.renlist.order_id,
+        amount: that.data.amount
+      },
+      success: resda => {
+        wx.requestPayment({
+          timeStamp: resda.Payment.timeStamp,
+          nonceStr: resda.Payment.nonceStr,
+          package: resda.Payment.package,
+          signType: resda.Payment.signType,
+          paySign: resda.Payment.paySign,
+          success (res) {
+            console.log(res, "成功")
+            that.pay(resda.data)
+          },
+          fail (res) {
+            wx.showToast({
+              title: '支付失败',
+              icon: 'nona',
+              duration: 2000
+            })
+            console.log(res, "失败")
+          }
+        })
+        console.log(res, "111")
 
+      }
+    })
+  },
+  pay (dataa) {
+    api.post({
+      url: `/Order/order_amount_payment`,
+      data: {
+        user_id: wx.getStorageSync('userid'),
+        user_token: wx.getStorageSync('token'),
+        order_id: dataa.order_id,
+        master_user_id: dataa.master_user_id,
+      },
+      success: resa => {
+        if (resa.code == 200) {
+          wx.showToast({
+            title: '支付成功',
+            icon: 'success',
+            duration: 2000
+          })
+          setTimeout(() => {
+            wx.navigateBack({
+              delta: 1
+            })
+          }, 2000)
+        }
+        console.log(resa, "zhifu")
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
